12771088e25088ceaa6f360edcb18a896fe2ad46
iMac CI&T <garagem@ciandt.com>
2015-04-07 08:27:48 -0300

    Add Temperature/Humidity to status

--- app/scripts/controllers/status.js
+++ app/scripts/controllers/status.js
@@ -27,12 +27,16 @@ angular.module('dashApp')
           else {
             sensorStatus = 'glyphicon glyphicon-remove'; // yellow
           }
+
+          console.log(data[0].body);
           r = {'location':     value.location,
                'label':        value.label,
                'deviceNumber': value.deviceNumber,
                'HwName' :      data[0].body.device,
                'lastDate':     mySensors.getData(data[0].created_at, true).split('|')[0],
                'lastTime':     mySensors.getData(data[0].created_at).split('|')[1],
+               'temperature':  data[0].body.data.Temperature,
+               'humidity':     data[0].body.data.Humidity,
                'status'  :     sensorStatus
               };
         $scope.statusResult.push(r);
